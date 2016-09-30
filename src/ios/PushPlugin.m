f05826cdba46360946ca9362e6d7a785de9d2d04
Beat <Beat.Grabe@hartwig-at.de>
2016-09-30 12:05:18 +0200

    [HOTFIX] Cast WebView

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -218,7 +218,11 @@
         NSLog(@"Msg: %@", jsonStr);
 
         NSString * jsCallBack = [NSString stringWithFormat:@"%@(%@);", self.callback, jsonStr];
-        [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
+        
+		// http://stackoverflow.com/a/34901862/5159481
+		if ([self.webView isKindOfClass:[UIWebView class]]) {
+			[(UIWebView*)self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
+		}
 
         self.notificationMessage = nil;
     }
