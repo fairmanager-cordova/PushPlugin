2e6ea359154c840d1603f5512497b0f0dc1559e1
Oliver Salzburg <oliver.salzburg@gmail.com>
2018-07-09 12:08:36 +0200

    [TASK] Cleanup

--- .scripts/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-plugman uninstall --platform android --project "/c/Users/OliverSalzburg/Fairmanager Projects/mobile2/platforms/android" --plugin /d/plugin-push
-plugman install --platform android --project "/c/Users/OliverSalzburg/Fairmanager Projects/mobile2/platforms/android" --plugin /d/plugin-push
