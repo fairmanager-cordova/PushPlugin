39da3c41f2cca08040ca543fef794293df1ef89f
Oliver Salzburg <oliver.salzburg@gmail.com>
2018-09-24 16:45:14 +0200

    [FIX] More cleanup

--- www/push.js
+++ www/push.js
@@ -1,18 +1,18 @@
-var PushNotification = function() {};
+function Push() {};
 
 // Call this to register for push notifications. Content of [options] depends on whether we are working with APNS (iOS) or GCM (Android)
-PushNotification.prototype.register = function( successCallback, errorCallback, options ) {
+Push.prototype.register = function( successCallback, errorCallback, options ) {
 	if( errorCallback == null ) {
 		errorCallback = function() {}
 	}
 
 	if( typeof errorCallback != "function" ) {
-		console.log( "PushNotification.register failure: failure parameter not a function" );
+		console.log( "Push.register failure: failure parameter not a function" );
 		return
 	}
 
 	if( typeof successCallback != "function" ) {
-		console.log( "PushNotification.register failure: success callback parameter must be a function" );
+		console.log( "Push.register failure: success callback parameter must be a function" );
 		return
 	}
 
@@ -20,18 +20,18 @@ PushNotification.prototype.register = function( successCallback, errorCallback, 
 };
 
 // Call this to unregister for push notifications
-PushNotification.prototype.unregister = function( successCallback, errorCallback, options ) {
+Push.prototype.unregister = function( successCallback, errorCallback, options ) {
 	if( errorCallback == null ) {
 		errorCallback = function() {}
 	}
 
 	if( typeof errorCallback != "function" ) {
-		console.log( "PushNotification.unregister failure: failure parameter not a function" );
+		console.log( "Push.unregister failure: failure parameter not a function" );
 		return
 	}
 
 	if( typeof successCallback != "function" ) {
-		console.log( "PushNotification.unregister failure: success callback parameter must be a function" );
+		console.log( "Push.unregister failure: success callback parameter must be a function" );
 		return
 	}
 
@@ -39,31 +39,32 @@ PushNotification.prototype.unregister = function( successCallback, errorCallback
 };
 
 // Call this if you want to show toast notification on WP8
-PushNotification.prototype.showToastNotification = function( successCallback, errorCallback, options ) {
+Push.prototype.showToastNotification = function( successCallback, errorCallback, options ) {
 	if( errorCallback == null ) {
 		errorCallback = function() {}
 	}
 
 	if( typeof errorCallback != "function" ) {
-		console.log( "PushNotification.register failure: failure parameter not a function" );
+		console.log( "Push.register failure: failure parameter not a function" );
 		return
 	}
 
 	require( "cordova/exec" )( successCallback, errorCallback, "Push", "showToastNotification", [ options ] );
 }
+
 // Call this to set the application icon badge
-PushNotification.prototype.setApplicationIconBadgeNumber = function( successCallback, errorCallback, badge ) {
+Push.prototype.setApplicationIconBadgeNumber = function( successCallback, errorCallback, badge ) {
 	if( errorCallback == null ) {
 		errorCallback = function() {}
 	}
 
 	if( typeof errorCallback != "function" ) {
-		console.log( "PushNotification.setApplicationIconBadgeNumber failure: failure parameter not a function" );
+		console.log( "Push.setApplicationIconBadgeNumber failure: failure parameter not a function" );
 		return
 	}
 
 	if( typeof successCallback != "function" ) {
-		console.log( "PushNotification.setApplicationIconBadgeNumber failure: success callback parameter must be a function" );
+		console.log( "Push.setApplicationIconBadgeNumber failure: success callback parameter must be a function" );
 		return
 	}
 
@@ -75,5 +76,5 @@ PushNotification.prototype.setApplicationIconBadgeNumber = function( successCall
 //-------------------------------------------------------------------
 
 if( typeof module != 'undefined' && module.exports ) {
-	module.exports = PushNotification;
+	module.exports = Push;
 }
